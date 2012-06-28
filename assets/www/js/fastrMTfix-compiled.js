6dfd01ece422d9cfa1006ded050c1efd4c5c5add
fastr <strachanski@googlemail.com>
2012-06-28 20:12:21 +0200

    updated fastrMTfix-compiled.js

--- assets/www/js/fastrMTfix-compiled.js
+++ assets/www/js/fastrMTfix-compiled.js
@@ -1,13 +1,3 @@
-var e=null;/*
-
- This work is licensed under the Creative Commons Attribution 3.0 Unported License. 
- To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/ 
- or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-
- Author: Fabian Strachanski
- Version: 0.1
-*/
-function f(){this.b=[];this.d=1.03;this.e=1.11}window.fastrMTfix=new f;function g(b,c,d,a,l){var i=window.fastrMTfix.b[0]/window.innerWidth,j=window.fastrMTfix.b[1]/window.innerHeight;this.identifier=b;this.target=e;this.screenX=c/i*window.fastrMTfix.d||e;this.screenY=d/j*window.fastrMTfix.e||e;this.pageX=a/i*window.fastrMTfix.d||e;this.pageY=l/j*window.fastrMTfix.e||e;this.target==e&&(this.target=document.elementFromPoint(this.screenX,this.screenY))}
-f.prototype.setDisplay=function(b,c){this.b=[b,c]};function h(b){for(var b=b||[],c,d=0;d<b.length;d++)c=b[d],this.push(new g(c.id,c.x,c.y,c.x,c.y))}h.prototype=[];function k(b,c){for(var d=0;d<b.length;d++)if(b[d].identifier==c)return b[d]}
-function m(b,c,d){var a=document.createEvent("Event");a.initEvent(b,!0,!0);a.touches=new h(d);a.targetTouches=new h(d);a.changedTouches=new h([]);a.changedTouches.push(k(a.touches,c));a.altKey=!1;a.metaKey=!1;a.ctrlKey=!1;a.shiftKey=!1;a.c=a.changedTouches[0].target;a.a=function(){this.c?this.c.dispatchEvent(this):document.dispatchEvent(this)};return a}f.prototype.touchstart=function(b,c,d,a){(new m("touchstart",b,a)).a()};f.prototype.touchmove=function(b,c,d,a){(new m("touchmove",b,a)).a()};
-f.prototype.touchend=function(b,c,d,a){(new m("touchend",b,a)).a()};
+function e(){console.log("LOADED")}window.fastrMTfix=new e;function f(b,d,c,a,i){this.identifier=b;this.target=null;this.screenX=d;this.screenY=c;this.pageX=a;this.pageY=i;this.clientX=d;this.clientY=c;null==this.target&&(this.target=document.elementFromPoint(this.screenX,this.screenY))}e.prototype.setDisplay=function(){};function g(b){for(var b=b||[],d,c=0;c<b.length;c++)d=b[c],this.push(new f(d.id,d.x,d.y,d.x,d.y))}g.prototype=[];
+function h(b,d){for(var c=0;c<b.length;c++)if(b[c].identifier==d)return b[c]}function j(b,d,c){var a=document.createEvent("Event");a.initEvent(b,!0,!0);a.touches=new g(c);a.targetTouches=new g(c);a.changedTouches=new g(c);a.altKey=!1;a.metaKey=!1;a.ctrlKey=!1;a.shiftKey=!1;a.b=h(a.touches,d).target;a.b=a.changedTouches[0].target;a.a=function(){if(this.b)for(var a=0;a<this.changedTouches.length;a++)this.changedTouches[a].target.dispatchEvent(this);else document.dispatchEvent(this)};return a}
+e.prototype.touchstart=function(b,d,c,a){(new j("touchstart",b,a)).a()};e.prototype.touchmove=function(b,d,c,a){(new j("touchmove",b,a)).a()};e.prototype.touchend=function(b,d,c,a){(new j("touchend",b,a)).a()};
